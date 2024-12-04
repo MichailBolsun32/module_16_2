d0b0a9db2a2b62012e489fd8db81f68cdb9577f1
MichailBolsun32 <bolsun32@mail.ru>
2024-12-04 04:40:56 +0300

    Add files via upload

---
+++ module_16_2.py
@@ -0,0 +1,45 @@
+from fastapi import FastAPI, Path
+from typing import Annotated
+
+app = FastAPI()
+
+@app.get("/")
+async def get_main_page() -> str:
+   return "Главная страница"
+
+@app.get("/user/admin")
+async def get_admin_page() -> str:
+    return "Вы вошли как администратор"
+
+# Допишите валидацию для маршрутов из предыдущей задачи при помощи классов Path и Annotated:
+        # '/user/{user_id}' - функция, выполняемая по этому маршруту, принимает аргумент user_id,
+        # для которого необходимо написать следующую валидацию:
+#     Должно быть целым числом
+#     Ограничено по значению: больше или равно 1 и меньше либо равно 100.
+#     Описание - 'Enter User ID'
+#     Пример - '1' (можете подставить свой пример не противоречащий валидации)
+
+@app.get("/user/{user_id}")
+async def get_user_number(user_id: Annotated[int, Path(ge=1, le=100, description='Enter User ID')]) -> dict:
+    return {"message": f"Вы вошли как пользователь № {user_id}"}
+
+
+# '/user' замените на '/user/{username}/{age}' - функция, выполняемая по этому маршруту,
+    # принимает аргументы username и age, для которых необходимо написать следующую валидацию:
+#     username - строка, age - целое число.
+#     username ограничение по длине: больше или равно 5 и меньше либо равно 20.
+#     age ограничение по значению: больше или равно 18 и меньше либо равно 120.
+#     Описания для username и age - 'Enter username' и 'Enter age' соответственно.
+#     Примеры для username и age - 'UrbanUser' и '24' соответственно. (можете подставить свои примеры не противоречащие валидации).
+
+@app.get("/user/{username}/{age}")
+async def get_user_info(username: Annotated[str, Path(min_length=5,
+                                             max_length=20,
+                                             description='Enter username',
+                                             example='UrbanUser')] ,
+                        age: int=Path(ge=18,
+                                      le=120,
+                                      description='Enter age',
+                                      example='24'
+                                      )) -> dict:
+    return {"message": f"Информация о пользователе. Имя: {username}, Возраст: {age}"}
